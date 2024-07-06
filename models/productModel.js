5a4bfac264c49cfe4eb3de7a4f2d1d7c05b8d45d
G M Ganesh <ganesh.gm@techo2.com>
2024-07-06 10:06:56 +0530

    Profile edit added

--- models/productModel.js
+++ models/productModel.js
@@ -1,58 +1,64 @@
-const { default: mongoose } = require("mongoose");
-const reviewSchema = mongoose.Schema({
+const mongoose = require("mongoose");
+
+const reviewSchema = new mongoose.Schema(
+  {
     userid: {
-        type: mongoose.Schema.Types.ObjectId
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
     },
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     comment: {
-        type: String,
+      type: String,
     },
     rating: {
-        type: Number,
-        required: true
-    }
-},
-    {
-        timeStamps: true
-    }
-)
-const productSchema = mongoose.Schema({
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const productSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     image: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
-    catergory: {
-        type: String,
-        required: true
+    category: {
+      type: String,
+      required: true,
     },
     description: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     price: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
     countInStock: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
     rating: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
-    reviews: [reviewSchema]
-},
-{
-    timeStamps: true
-})
+    reviews: [reviewSchema],
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const Product=mongoose.model("products",productSchema);
-module.exports = Product
+const Product = mongoose.model("Product", productSchema);
+module.exports = Product;
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,22 +1,44 @@
-const express=require('express');
-const Product = require('../models/productModel');
-const router=express.Router()
+const express = require("express");
+const Product = require("../models/productModel");
+const router = express.Router();
 
-router.get("/",async(req,res)=>{
-    try {
-        const prodcuts=await Product.find({})
-        return res.status(200).json(prodcuts)
-    } catch (err) {
-        return res.status(400).json(err)
-    }
-})
-router.post("/:id",async(req,res)=>{
-    try {
-        const prodcut=await Product.findById(req.params.id)
-        return res.status(200).json(prodcut)
-    } catch (err) {
-        return res.status(400).json(err)
-    }
-})
+router.get("/", async (req, res) => {
+  try {
+    const prodcuts = await Product.find({});
+    return res.status(200).json(prodcuts);
+  } catch (err) {
+    return res.status(400).json(err);
+  }
+});
+router.post("/:id", async (req, res) => {
+  try {
+    const prodcut = await Product.findById(req.params.id);
+    return res.status(200).json(prodcut);
+  } catch (err) {
+    return res.status(400).json(err);
+  }
+});
+
+router.post('/revie', async (req, res) => {
+  try {
+      const { review, productid, currentUser } = req.body;
+      const id= new ObjectId(productid)
+      const product = await Product.findById(productid);
+      const userreview = {
+          name: currentUser.name,
+          userid: mongoose.Types.ObjectId(currentUser._id),
+          rating: review.rating,
+          comment: review.comment,
+      };
+
+      product.reviews.push(userreview);
+      await product.save();
+
+      return res.status(200).json('Product Review Saved Successfully');
+  } catch (err) {
+      console.error(err);  // Log the error for debugging purposes
+      return res.status(400).json({ error: err.message });
+  }
+});
 
 module.exports = router;
